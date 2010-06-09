f11cda25e3fe72103f0d41314fc56ad6ca85d2c0
Dirk Bergmann <dirk@dirk.net>
2010-06-09 23:32:36 +0700

    Refactored javascript to inline before and after functions

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,19 +1,14 @@
-function onBefore() {
-    $(this).children("dd").hide();
-}
-
-function onAfter() {
-   // $("dd").fadeOut("slow");
-   $(this).children("dd").fadeIn("slow");
-}
-$(document).ready(function(){
-     
+$(document).ready(function(){  
     $('#captioned-images').show();
     $('#captioned-images').cycle({
         fx: 'scrollLeft',
         timeout: 5000,
-        after: onAfter,
-        before: onBefore       
-    });   
-});
-        
+        after: function() {
+            // $("dd").fadeOut("slow");
+            $(this).children("dd").fadeIn("slow");
+        },
+        before: function () {
+            $(this).children("dd").hide();
+        }
+    });
+}); 
